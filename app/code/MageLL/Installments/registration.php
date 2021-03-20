74bbf7e39675ae942ae010a2ca264c0fe60466d7
Leandro Rosa <dev.leandrorosa@gmail.com>
2021-03-19 22:59:29 -0300

    Initital commit

---
+++ app/code/MageLL/Installments/registration.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Trezo Soluções Web
+ *
+ * NOTICE OF LICENSE
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to https://www.trezo.com.br for more information.
+ *
+ * @category Trezo
+ *
+ * @copyright Copyright (c) 2021 Trezo Soluções Web. (https://www.trezo.com.br)
+ *
+ * @author Trezo Core Team <contato@trezo.com.br>
+ */
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'MageLL_Installments', __DIR__);
+
